b4799a296561e7866622462146b7316607e0e464
Ghost_chu <2908803755@qq.com>
2022-01-11 15:56:38 +0800

    Optimize broadcast performance

--- src/main/java/com/mcsunnyside/coreprotecttnt/Util.java
+++ src/main/java/com/mcsunnyside/coreprotecttnt/Util.java
@@ -1,22 +1,29 @@
 package com.mcsunnyside.coreprotecttnt;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.configuration.Configuration;
 import org.bukkit.configuration.ConfigurationSection;
-import org.bukkit.entity.Player;
+import org.bukkit.util.Vector;
 
 public class Util {
     public static void broadcastNearPlayers(Location location, String message) {
         if (message == null || message.isEmpty()) {
             return; // Do not send empty message
         }
-        //noinspection ConstantConditions
-        location.getWorld().getNearbyEntities(location, 15, 15, 15).forEach(entity -> {
-            if (entity instanceof Player) {
-                entity.sendMessage(ChatColor.translateAlternateColorCodes('&', message));
-            }
-        });
+        Bukkit.getOnlinePlayers().forEach(
+                player -> {
+                    if(!player.getWorld().equals(location.getWorld()))
+                        return;
+
+                    Vector playerVector = player.getLocation().toVector();
+                    Vector centralVector = location.toVector();
+                   if(playerVector.distance(centralVector) < 15){
+                       player.sendMessage(ChatColor.translateAlternateColorCodes('&', message));
+                   }
+                }
+        );
     }
 
     public static ConfigurationSection bakeConfigSection(Configuration configuration, String path) {
