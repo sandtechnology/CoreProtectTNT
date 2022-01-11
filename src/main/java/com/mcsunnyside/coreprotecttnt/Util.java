baa77283ad95cca9bc412ee0ac9c182032172e7d
Ghost_chu <2908803755@qq.com>
2022-01-11 16:00:12 +0800

    More optimize

--- src/main/java/com/mcsunnyside/coreprotecttnt/Util.java
+++ src/main/java/com/mcsunnyside/coreprotecttnt/Util.java
@@ -1,10 +1,10 @@
 package com.mcsunnyside.coreprotecttnt;
 
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.configuration.Configuration;
 import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.entity.Player;
 import org.bukkit.util.Vector;
 
 public class Util {
@@ -12,18 +12,14 @@ public class Util {
         if (message == null || message.isEmpty()) {
             return; // Do not send empty message
         }
-        Bukkit.getOnlinePlayers().forEach(
-                player -> {
-                    if(!player.getWorld().equals(location.getWorld()))
-                        return;
-
-                    Vector playerVector = player.getLocation().toVector();
-                    Vector centralVector = location.toVector();
-                   if(playerVector.distance(centralVector) < 15){
-                       player.sendMessage(ChatColor.translateAlternateColorCodes('&', message));
-                   }
-                }
-        );
+        String msg = ChatColor.translateAlternateColorCodes('&', message);
+        for (Player player : location.getWorld().getPlayers()) {
+            Vector playerVector = player.getLocation().toVector();
+            Vector centralVector = location.toVector();
+            if(playerVector.distanceSquared(centralVector) < 15){
+                player.sendMessage(msg);
+            }
+        }
     }
 
     public static ConfigurationSection bakeConfigSection(Configuration configuration, String path) {
